6b5e17d6f08685f3878d718cbdff786c2e4d0274
Mohammad Shahmeer <mohammad.shahmeer@confiz.local>
2024-05-21 15:31:59 +0500

    connected to DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server is running on 5000");
-});
+mongoose
+  .connect(`${process.env.MONGO}`)
+  .then((result) => {
+    app.listen(5000, () => {
+      console.log(`Server is running on 5000 and Connected to DB!`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
